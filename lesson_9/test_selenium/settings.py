a1fa3ad72d53c0cbf7d34bf6e08601a821293ae6
IlyaMuralha <muralhakuzmenko@gmail.com>
2021-11-09 20:37:00 +0500

    Added seleniummiddleware

--- lesson_9/test_selenium/settings.py
+++ lesson_9/test_selenium/settings.py
@@ -12,26 +12,30 @@ BOT_NAME = 'test_selenium'
 SPIDER_MODULES = ['test_selenium.spiders']
 NEWSPIDER_MODULE = 'test_selenium.spiders'
 
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG'
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'test_selenium (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -50,9 +54,14 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'test_selenium.middlewares.TestSeleniumDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'test_selenium.middlewares.TestSeleniumDownloaderMiddleware': 543,
+   'test_selenium.middlewares.SeleniumMiddleware': 800,
+}
+
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = './chromedriver.exe'
+SELENIUM_DRIVER_ARGUMENTS = ['start-maximized']
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -62,9 +71,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'test_selenium.pipelines.TestSeleniumPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'test_selenium.pipelines.TestSeleniumPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- lesson_9/test_selenium/spiders/hhru.py
+++ lesson_9/test_selenium/spiders/hhru.py
@@ -4,7 +4,7 @@ import scrapy
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['https://hh.ru/']
+    start_urls = ['https://hh.ru/search/vacancy?area=1&fromSearchLine=true&text=Python+junior&from=suggest_post']
 
     def parse(self, response):
         pass
